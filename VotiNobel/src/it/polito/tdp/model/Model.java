0d4cccf5a82741fb7b13cff2fd5d19fddbc4c5c4
Lore <Lore@172.20.242.42>
2018-04-26 17:27:07 +0200

    ricorsione con scelta del sottoinsieme ideale

--- VotiNobel/src/it/polito/tdp/model/Model.java
+++ VotiNobel/src/it/polito/tdp/model/Model.java
@@ -1,14 +1,92 @@
 package it.polito.tdp.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import it.polito.tdp.dao.EsameDAO;
+
 public class Model {
+	
+	private List <Esame> esami;
+	private EsameDAO edao;
+	private List <Esame> soluzione; 
+	private double bestAvg;
+	
+	
+	public Model () {
+		
+		edao= new EsameDAO();
+		esami= edao.getTuttiEsami();
+	}
 
 	public List<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
 		
-		System.out.println("TODO!");
+		soluzione = new ArrayList<>();
 		
-		return null;
+		bestAvg = 0;
+		
+		int step =0;
+		
+		List <Esame> parziale = new ArrayList<>();
+		
+		recursive(step, parziale, numeroCrediti);
+		
+		return soluzione;
 	}
 
+	private void recursive(int step, List<Esame> parziale, int numeroCrediti) {
+		
+		if( totCrediti(parziale) > numeroCrediti ) {
+					
+			return;
+			
+		}
+		
+		if(totCrediti(parziale) == numeroCrediti) {
+			
+			if( avg(parziale) > bestAvg) {
+				
+				bestAvg=avg(parziale);
+				
+				soluzione = new ArrayList<>(parziale);
+			}
+		}
+		
+		for (Esame e : esami) {
+			
+			if(!parziale.contains(e)) {
+				
+				parziale.add(e);
+				
+				recursive(step+1, parziale, numeroCrediti);
+				
+				parziale.remove(e);
+				
+			}
+		}
+	}
+
+	private double avg(List<Esame> parziale) {
+		
+		double avg=0;
+		
+		for (Esame e : parziale)
+			
+			avg += e.getCrediti() * e.getVoto();
+		
+		avg /= this.totCrediti(parziale);
+		
+		return avg;
+	}
+
+	private int totCrediti(List<Esame> parziale) {
+		
+		int somma=0;
+		
+		for( Esame e : parziale)
+			
+			somma+= e.getCrediti();
+		
+		return somma;
+	}
 }
--- VotiNobel/src/it/polito/tdp/model/TestModel.java
+++ VotiNobel/src/it/polito/tdp/model/TestModel.java
@@ -1,13 +1,19 @@
 package it.polito.tdp.model;
 
+import java.util.List;
+
 public class TestModel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Model model = new Model();
-		model.calcolaSottoinsiemeEsami(20);
+		List <Esame> soluzione = model.calcolaSottoinsiemeEsami(22);
 		
+		
+		for(Esame e : soluzione)
+			
+			System.out.println(e);
 	}
 
 }
